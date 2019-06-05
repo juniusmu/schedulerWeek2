32effef0771c7183bee5b6a759c6cddd83ad17b0
vjotchere <vjotchere@gmail.com>
2019-06-05 18:16:03 -0400

    client class and controller

--- controllers/client_controller.rb
+++ controllers/client_controller.rb
@@ -5,4 +5,16 @@ require 'tty-prompt'
 require_relative '../models/client'
 
 class ClientController
+    attr_accessor :clients
+
+    @clients = [Client.new(0, 'Vlad', [Appointment.new('Bob', 'Test Service 1', 'Test', Date.new(2020,1,1), 12)]),
+                Client.new(1, 'Eddie', [Appointment.new('Bobby', 'Test Service 1', 'Test', Date.new(2020,2,1), 12)])]
+
+    def self.all
+        @clients
+    end
+
+    def self.add
+    
+    end
 end
--- models/client.rb
+++ models/client.rb
@@ -1,6 +1,7 @@
 class Client
-	attr_accessor :name, :appointments
-	def initialize(name, appointments = [])
+	attr_accessor :id, :name, :appointments
+	def initialize(id, name, appointments = [])
+		@id = id
 		@name = name
 		@appointments = appointments
 	end
