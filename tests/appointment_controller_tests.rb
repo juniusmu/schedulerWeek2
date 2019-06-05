9b8e70992d4397d925e79fe84f69fb6cbe49b45f
vjotchere <vjotchere@gmail.com>
2019-06-05 18:28:06 -0400

    created remove appointment test

--- tests/appointment_controller_tests.rb
+++ tests/appointment_controller_tests.rb
@@ -7,11 +7,27 @@ require '../controllers/appointment_controller'
 RSpec.describe AppointmentController do 
     describe "#add_appointment" do
         it "add an appointment" do
+            # need to create a provider when provider_controller is refactored
             providers = ProviderController.send :all
             AppointmentController.send :add_appointment, 'Bob', 'Test Service 1', providers[0], Date.new(2020,1,1), 12
             appointments = AppointmentController.send :all
 
             expect(appointments.size()).to eq(1)
+        end
+    end
+
+    describe "#remove_appointment" do
+        it "removes an appointment" do
+            # need to create a provider when provider_controller is refactored
+            providers = ProviderController.send :all
+            AppointmentController.send :add_appointment, 'Bob', 'Test Service 1', providers[0], Date.new(2020,1,1), 12
+            
+            AppointmentController.send :remove_appointment
+            appointments = AppointmentController.send :all
+
+            expect(appointments.size()).to eq(0)
+            # check provider appointments array
+            # check clients appointments array
         end
     end
 end
