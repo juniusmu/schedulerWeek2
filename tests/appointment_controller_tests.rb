aacb16aed2266febdcd92f2dad7ee3321729de69
vjotchere <vjotchere@gmail.com>
2019-06-06 09:59:15 -0400

    added remove appt tests

--- tests/appointment_controller_tests.rb
+++ tests/appointment_controller_tests.rb
@@ -3,31 +3,54 @@
 require '../controllers/service_controller'
 require '../controllers/provider_controller'
 require '../controllers/appointment_controller'
+require '../controllers/client_controller'
 
 RSpec.describe AppointmentController do 
     describe "#add_appointment" do
-        it "add an appointment" do
-            # need to create a provider when provider_controller is refactored
-            providers = ProviderController.send :all
-            AppointmentController.send :add_appointment, 'Bob', 'Test Service 1', providers[0], Date.new(2020,1,1), 12
-            appointments = AppointmentController.send :all
 
+        clients = ClientController.send :all
+        services = ServiceController.send :all
+        providers = ProviderController.send :all
+        appointments = AppointmentController.send :all
+
+        it "checks no appointments exist" do
+            expect(appointments.size()).to eq(0)
+            expect(clients[0].appointments.size()).to eq(1)
+            expect(providers[0].scheduled_appointments.size()).to eq(0)
+        end
+        it "adds an appointment to appointment controller" do
+            AppointmentController.send :add_appointment, clients[0], services[0], providers[0], Date.new(2020,1,1), 12
             expect(appointments.size()).to eq(1)
+        end
+        it "check client controller for added appointment" do
+            expect(clients[0].appointments.size()).to eq(1)
+        end
+        it "check provider controller for added appointment" do
+            expect(providers[0].scheduled_appointments.size()).to eq(1)
         end
     end
 
     describe "#remove_appointment" do
-        it "removes an appointment" do
-            # need to create a provider when provider_controller is refactored
-            providers = ProviderController.send :all
-            AppointmentController.send :add_appointment, 'Bob', 'Test Service 1', providers[0], Date.new(2020,1,1), 12
-            
-            AppointmentController.send :remove_appointment
-            appointments = AppointmentController.send :all
 
+        clients = ClientController.send :all
+        services = ServiceController.send :all
+        providers = ProviderController.send :all
+        appointments = AppointmentController.send :all
+
+        it "adds an appointment" do
+            # expect(appointments.size()).to eq(0)    
+            # AppointmentController.send :add_appointment, clients[0], services[0], providers[0], Date.new(2020,1,1), 12
+            expect(appointments.size()).to eq(1)
+        end
+        it "removes the appointment" do
+            AppointmentController.send :remove_appointment, clients[0], services[0], providers[0], Date.new(2020,1,1), 12
             expect(appointments.size()).to eq(0)
-            # check provider appointments array
-            # check clients appointments array
+        end
+        it "checks client controller" do
+            expect(clients[0].appointments.size()).to eq(0)
+        end
+        it "checks provider controller" do
+            expect(providers[0].scheduled_appointments.size()).to eq(0)
         end
     end
 end
