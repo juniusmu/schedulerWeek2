1c2a86355dc3914c5e6911b30d63d078a1f2b5e4
vjotchere <vjotchere@gmail.com>
2019-06-07 09:05:52 -0400

    removed appointments from hardcoded clients

--- controllers/client_controller.rb
+++ controllers/client_controller.rb
@@ -7,8 +7,8 @@ require_relative '../models/client'
 class ClientController
     attr_accessor :clients
 
-    @clients = [Client.new(0, 'Vlad', [Appointment.new('Bob', 'Test Service 1', 'Test', Date.new(2020,1,1), 12)]),
-                Client.new(1, 'Eddie', [Appointment.new('Bobby', 'Test Service 1', 'Test', Date.new(2020,2,1), 12)])]
+    @clients = [Client.new(0, 'Vlad', []),
+                Client.new(1, 'Eddie', [])]
 
     def self.all
         @clients
