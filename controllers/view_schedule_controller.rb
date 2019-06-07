9a32ac3eabb53760774bab8ab6d44115ce0dc4d1
Eddie Taliaferro <eddielacrosse2@gmail.com>
2019-06-07 08:49:03 -0400

    Last Commit.

--- controllers/view_schedule_controller.rb
+++ controllers/view_schedule_controller.rb
@@ -1,22 +1,28 @@
 require 'tty-prompt'
 require 'colorize'
-# require '../models/schedule'
 require_relative './provider_controller'
-provider_services = %w(One Two Three)
+require_relative './appointment_controller'
 
-def view_schedule
-  prompt = TTY::Prompt.new(interrupt: :exit)
-  # Let the user know what they will be doing
-  prompt.say('----------')
-  prompt.say("Let's view a schedule for a Service Provider.\nFirst you'll select a Provider, a Day, then a Service with that Provider.")
-  prompt.say('----------')
+class ViewScheduleController
+  def self.view_schedule
+    appointments = AppointmentController.all
+    puts appointments
+    prompt = TTY::Prompt.new(interrupt: :exit)
+    # Let the user know what they will be doing
+    prompt.say('----------')
+    prompt.say("Let's view a schedule for a Service Provider.\nFirst you'll select a Provider, a Day, then a Service with that Provider.")
+    prompt.say('----------')
 
-  # Get Provider
-  provider = prompt.select("Choose a Service Provider (hit control + c to exit)", %w(Junius Eddie Vlad))
-  day = prompt.select("Choose a day of the week (hit control + c to exit)", %w(Monday Tuesday Wendesday Thursday Friday Saturday Sunday))
-  services = prompt.multi_select("Select a service for this day", %w(One Two Three))
+    # Get Provider
+    provider = prompt.select("Choose a Service Provider (hit control + c to exit)", %w(Junius Eddie Vlad))
 
+    # Get Day
+    day = prompt.select("Choose a day of the week (hit control + c to exit)", %w(Monday Tuesday Wendesday Thursday Friday Saturday Sunday))
 
+    # Get Services for each Provider on a particular day
+    appointments = AppointmentController
+    puts appointments
+    
+    services = prompt.multi_select("Select a service for this day", %w(One Two Three))
+  end
 end
-
-view_schedule()
